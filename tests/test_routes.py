e67180956c6fa2d73e0cdc8447881409f0c8a0f7
Rafael Monterroza <rafaelenrike@outlook.com>
2024-05-03 14:15:32 -0400

    Fixed blank lines in test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,7 +138,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(data["name"], account.name)
-    
+
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
@@ -177,7 +177,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         data = resp.get_json()
         self.assertEqual(len(data), 5)
-    
+
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
